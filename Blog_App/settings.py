7477398b940a15edd9dac8a10963f7e07891323d
Moneesh <moneeshkumar95@gmail.com>
2021-03-28 21:27:28 +0530

    Added

--- Blog_App/settings.py
+++ Blog_App/settings.py
@@ -130,3 +130,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'techsamy1803@gmail.com'
+EMAIL_HOST_PASSWORD = 'Forever555#'
+
+
--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import  views
 from .models import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,5 +12,9 @@ urlpatterns = [
     path('publish_blog',views.publishblog, name='publish_blog'),
     path('my_blogs/edit_blog/<int:pk>',views.editblog, name= 'edit_blog'),
     path('my_blogs/delete_blog/<int:pk>',views.deleteblog, name= 'delete_blog'),
-    path('blog/<str:pk>/', views.detailview, name= 'detailview')
+    path('blog/<str:pk>/', views.detailview, name= 'detailview'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name= 'password_reset'),
+    path('password_reset_sent/',auth_views.PasswordResetDoneView.as_view(template_name= 'password_reset_sent.html'), name= 'password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name= 'password_reset_confirm.html'), name= 'password_reset_confirm'),
+    path('password_reset/complete/',auth_views.PasswordResetCompleteView.as_view(template_name= 'password_reset_complete.html'), name= 'password_reset_complete'),
 ]
